1936c1e647e4eacd85b6bda847c1fd63cf9cba1e
Rafael Valdez <valdezr1@outlook.com>
2018-06-15 13:17:45 -0700

    random values determined through private helper method and converted weights array to be dynamically allocated

--- Perceptron.h
+++ Perceptron.h
@@ -11,26 +11,54 @@ public:
 	//Defualt Constructor
 	Perceptron(){
 		cout << "Hello Perceptron | Default" << endl;
-		weightSize = 0;
-	}
-
-	//Constructor
-	Perceptron(const int weightSize) {
-		cout << "Hello Perceptron" << endl;
-		this->weightSize = weightSize;
+		weightCapacity = defaultMaxWeightSize; //Default of 2
+		weights = new float[weightCapacity];
 
 		//Initialize Weights Randomly
-		float Low = -1.0, High = 1.0, randomValue;
-		for (int i = 0; i < weightSize; i++) {
+		float randomValue;
+		for (int i = 0; i < weightCapacity; i++) {
 			// Generate psuedo-random value from -1 - 1
-			randomValue = Low + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (High - Low)));
+			randomValue = RandomFloat(weightLow, weightHigh);
 			weights[i] = randomValue;
+			weightSize++;
 		}
 	}
 
+	//Constructor
+	Perceptron(const int maxWeightSize) {
+		cout << "Hello Perceptron" << endl;
+		weightCapacity = maxWeightSize;
+		weights = new float[weightCapacity];
+
+		//Initialize Weights Randomly
+		float randomValue;
+		for (int i = 0; i < weightCapacity; i++) {
+			// Generate psuedo-random value from -1 - 1
+			randomValue = RandomFloat(weightLow, weightHigh);
+			weights[i] = randomValue;
+			weightSize++;
+		}
+	}
+
+	//Destructor 
+	~Perceptron() {
+		delete[] weights;
+	}
+
 private:
-	static const int WEIGHT_MAX_SIZE = 2;
-	float weights[WEIGHT_MAX_SIZE];
+	float RandomFloat(float a, float b) {
+		float random = ((float)rand()) / (float)RAND_MAX;
+		float diff = b - a;
+		float r = random * diff;
+		return a + r;
+	}
+
+	const int defaultMaxWeightSize = 2;
+	const float weightLow = -1.0;
+	const float weightHigh = 1.0;
+
+	float* weights;
+	int weightCapacity;
 	int weightSize;
 
 };
